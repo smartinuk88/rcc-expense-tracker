6de541089a0cea3f067c065a70113b1b89975cb2
Scott Martin <scott.martin88@live.com>
2023-10-18 11:16:36 +0200

    Create HTML and CSS for NewExpense form. Create newExpense and ExpenseForm components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense";
 
 function App() {
   const expenses = [
@@ -26,6 +27,7 @@ function App() {
 
   return (
     <div>
+      <NewExpense />
       <Expenses expenses={expenses} />
     </div>
   );
---
+++ src/components/ExpenseForm.js
@@ -0,0 +1,27 @@
+import "./ExpenseForm.css";
+
+function ExpenseForm() {
+  return (
+    <form>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label htmlFor="title">Title</label>
+          <input type="text" />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="amount">Amount</label>
+          <input type="number" min={0.01} step={0.01} />
+        </div>
+        <div className="new-expense__control">
+          <label htmlFor="date">Date</label>
+          <input type="date" min="2019-01-01" max="2023-12-31" />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+}
+
+export default ExpenseForm;
